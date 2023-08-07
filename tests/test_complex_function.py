85e512405b06e24eae4da2a1fd8bf3b83d639d68
l1zp <lzpthu@163.com>
2023-08-07 23:53:49 +0800

    [WIP] add test

---
+++ tests/test_complex_function.py
@@ -0,0 +1,75 @@
+import numpy as np
+import unittest
+
+from pydiablo import Variable
+
+
+class SphereTest(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.x = Variable(np.array(3.0))
+        self.y = Variable(np.array(4.0))
+
+    def sphere(self, x, y):
+        z = x**2 + y**2
+        return z
+
+    def test_forward(self):
+        z = self.sphere(self.x, self.y)
+        expected = np.array(25.0)
+        self.assertEqual(z.data, expected)
+
+    def test_backward(self):
+        z = self.sphere(self.x, self.y)
+        z.backward()
+        expected_x = np.array(6.0)
+        expected_y = np.array(8.0)
+        self.assertEqual(self.x.grad, expected_x)
+        self.assertEqual(self.y.grad, expected_y)
+
+
+class MatyasTest(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.x = Variable(np.array(1.0))
+        self.y = Variable(np.array(1.0))
+
+    def matyas(self, x, y):
+        z = 0.26 * (x**2 + y**2) - 0.48 * x * y
+        return z
+
+    def test_forward(self):
+        z = self.matyas(self.x, self.y)
+        expected = np.array(0.040000000000000036)
+        self.assertEqual(z.data, expected)
+
+    def test_backward(self):
+        z = self.matyas(self.x, self.y)
+        z.backward()
+        expected_x = np.array(0.040000000000000036)
+        expected_y = np.array(0.040000000000000036)
+        self.assertEqual(self.x.grad, expected_x)
+        self.assertEqual(self.y.grad, expected_y)
+
+class GPTest(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.x = Variable(np.array(1.0))
+        self.y = Variable(np.array(1.0))
+
+    def goldstein(self, x, y):
+        z = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y))
+        return z
+
+    def test_forward(self):
+        z = self.matyas(self.x, self.y)
+        expected = np.array(0.040000000000000036)
+        self.assertEqual(z.data, expected)
+
+    def test_backward(self):
+        z = self.matyas(self.x, self.y)
+        z.backward()
+        expected_x = np.array(0.040000000000000036)
+        expected_y = np.array(0.040000000000000036)
+        self.assertEqual(self.x.grad, expected_x)
+        self.assertEqual(self.y.grad, expected_y)
