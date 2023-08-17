2230d7d565b2972ac3790339b56a972c88e35f85
l1zp <lzpthu@163.com>
2023-08-17 21:19:02 +0800

    add more function

---
+++ example/newton_method.py
@@ -0,0 +1,22 @@
+import numpy as np
+from pydiablo import Variable
+
+def f(x):
+    y = x ** 4 - 2 * x ** 2
+    return y
+
+if __name__ == "__main__":
+    x = Variable(np.array(2.0))
+    iters = 10
+    for i in range(iters):
+        print(i, x)
+        y = f(x)
+        x.cleargrad()
+        y.backward(create_graph=True)
+
+        gx = x.grad
+        x.cleargrad()
+        gx.backward()
+        gx2 = x.grad
+
+        x.data -= gx.data / gx2.data
